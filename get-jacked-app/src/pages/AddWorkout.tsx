25cd7df8f9cb56fc9ffdb1665161c88f6aa5e92c
damonryon <damon.ryon@gmail.com>
2025-02-13 08:40:38 -0700

    added day selector

--- get-jacked-app/src/pages/AddWorkout.tsx
+++ get-jacked-app/src/pages/AddWorkout.tsx
@@ -20,19 +20,127 @@ export const AddWorkout = () => {
         className="enter-workout"
         placeholder="Enter workout (ex. chest)"
       />
-      <div className="1-rep-box">
+      <div className="rep-box">
+        <span>Track 1 Rep Max?</span>
         <input
           type="radio"
-          id="1-rep-y"
-          name="1-rep"
+          id="rep-y"
+          name="rep"
+          className="rep-input"
         />
-        <label htmlFor="i-rep-y">Y</label>
+        <label
+          htmlFor="rep-y"
+          className="rep-label"
+        >
+          Y
+        </label>
         <input
           type="radio"
-          id="1-rep-n"
-          name="1-rep"
+          id="rep-n"
+          name="rep"
+          className="rep-input"
         />
-        <label htmlFor="i-rep-n">N</label>
+        <label
+          htmlFor="rep-n"
+          className="rep-label"
+        >
+          N
+        </label>
+      </div>
+      <div className="select-day-box">
+        <span>Select day to do this workout</span>
+        <div className="day-checker-box">
+          <input
+            type="radio"
+            id="sun"
+            name="day"
+            className="day-input"
+          />
+          <label
+            htmlFor="sun"
+            className="day-label"
+          >
+            S
+          </label>
+
+          <input
+            type="radio"
+            id="mon"
+            name="day"
+            className="day-input"
+          />
+          <label
+            htmlFor="mon"
+            className="day-label"
+          >
+            M
+          </label>
+
+          <input
+            type="radio"
+            id="tues"
+            name="day"
+            className="day-input"
+          />
+          <label
+            htmlFor="tues"
+            className="day-label"
+          >
+            T
+          </label>
+
+          <input
+            type="radio"
+            id="wed"
+            name="day"
+            className="day-input"
+          />
+          <label
+            htmlFor="wed"
+            className="day-label"
+          >
+            W
+          </label>
+
+          <input
+            type="radio"
+            id="thurs"
+            name="day"
+            className="day-input"
+          />
+          <label
+            htmlFor="thurs"
+            className="day-label"
+          >
+            T
+          </label>
+
+          <input
+            type="radio"
+            id="fri"
+            name="day"
+            className="day-input"
+          />
+          <label
+            htmlFor="fri"
+            className="day-label"
+          >
+            F
+          </label>
+
+          <input
+            type="radio"
+            id="sat"
+            name="day"
+            className="day-input"
+          />
+          <label
+            htmlFor="sat"
+            className="day-label"
+          >
+            S
+          </label>
+        </div>
       </div>
       <button onClick={handleAddWorkout}>Add Workout to Routine</button>
       <button onClick={handleBack}>Back</button>
