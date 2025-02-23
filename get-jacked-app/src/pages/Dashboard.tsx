268a97a55c9abaa88121cb299613bf2f370c06cf
damonryon <damon.ryon@gmail.com>
2025-02-23 07:45:02 -0700

    workouts expand to show movement

--- get-jacked-app/src/pages/Dashboard.tsx
+++ get-jacked-app/src/pages/Dashboard.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
   const [workoutList, setWorkoutList] = useState([]);
+  const [expandedWorkoutId, setExpandedWorkoutId] = useState<string | null>(
+    null
+  );
+  const [movementsList, setMovementsList] = useState([]);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -43,6 +47,30 @@ export const Dashboard = () => {
     fetchWorkouts();
   }, []);
 
+  const toggleExpand = (workoutId: string) => {
+    setExpandedWorkoutId((prev) => (prev === workoutId ? null : workoutId));
+  };
+
+  const getMovements = () => {
+    return fetch("http://localhost:3000/movements")
+      .then((res) => res.json())
+      .catch((err: Error) => {
+        throw new Error(`HTTP request getMovements failed ${err.message}`);
+      });
+  };
+
+  useEffect(() => {
+    const fetchMovements = async () => {
+      try {
+        const movements = await getMovements();
+        setMovementsList(movements);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchMovements();
+  }, []);
+
   return (
     <div>
       <img
@@ -59,12 +87,24 @@ export const Dashboard = () => {
           workoutList.map(
             (workout: { id: string; workout: string; day: string }) => (
               <div
+                onClick={() => toggleExpand(workout.id)}
                 className="workout-list-item"
                 key={workout.id}
               >
                 <p>
                   {workout.day} - {workout.workout}
                 </p>
+                {expandedWorkoutId === workout.id && (
+                  <div className="movements-list">
+                    {movementsList
+                      .filter((movement) => movement.workoutID === workout.id)
+                      .map((movement) => (
+                        <p key={movement.id}>
+                          {movement.movement} - {movement.sets} sets
+                        </p>
+                      ))}
+                  </div>
+                )}
               </div>
             )
           )
