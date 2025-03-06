2c64a316db8defd052052ff0d7979b766706990c
damonryon <damon.ryon@gmail.com>
2025-03-06 06:58:34 -0700

    building enterWeight component

--- get-jacked-app/src/pages/TodaysWorkout.tsx
+++ get-jacked-app/src/pages/TodaysWorkout.tsx
@@ -2,15 +2,21 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CreateWorkout } from "../context/CreateWorkout";
 
+interface Movement {
+  id: string;
+  movement: string;
+  sets: number;
+}
+
 export const TodaysWorkout = () => {
-  const [muscleGroup, setMuscleGroup] = useState("");
-  const [movements, setMovements] = useState("");
+  const [muscleGroup, setMuscleGroup] = useState<string>("");
+  const [movements, setMovements] = useState<Movement[]>([]);
   const workout = useContext(CreateWorkout);
   if (!workout) {
     throw new Error("workout is null");
   }
 
-  const { todaysWorkout, setTodaysWorkout } = workout;
+  const { todaysWorkout } = workout;
 
   const navigate = useNavigate();
 
@@ -26,8 +32,7 @@ export const TodaysWorkout = () => {
             `http://localhost:3000/workouts/${todaysWorkout}`
           );
           const workoutResponse = await response.json();
-          setMuscleGroup(workoutResponse);
-          console.log(muscleGroup);
+          setMuscleGroup(workoutResponse.workout);
           const moveResponse = await fetch(
             `http://localhost:3000/movements?workoutID=${todaysWorkout}`
           );
@@ -35,9 +40,11 @@ export const TodaysWorkout = () => {
           setMovements(moveResponseData);
         } catch (err: unknown) {
           if (err instanceof Error) {
-            throw new Error(
+            console.error(
               `HTTP request fetchTodaysWorkout failed: ${err.message}`
             );
+          } else {
+            console.error("HTTP request fetchTodaysWorkout failed");
           }
         }
       }
@@ -47,14 +54,23 @@ export const TodaysWorkout = () => {
 
   return (
     <div>
-      <h3 className="page-title">Today's Workout</h3>
-      <h2>5</h2>
+      <img
+        src="/today-workout.png"
+        className="page-title"
+        alt=""
+      />
+      <h2>{muscleGroup}</h2>
       <div>
         This is the Today's Workout page. Here you can see and perform today's
         workout.
       </div>
-      <div>{`muscle group ${muscleGroup}`}</div>
-      <div>{`movements ${movements}`}</div>
+      <div className="movements-list">
+        {movements.map((movement) => (
+          <p key={movement.id}>
+            {movement.movement} - {movement.sets} sets
+          </p>
+        ))}
+      </div>
       <button onClick={handleFinish}>Finish</button>
     </div>
   );
